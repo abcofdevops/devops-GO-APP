a819e0be2d65556033f10c7d3bd492d681c80aeb
ABCofDevops <abcofdevops@gmail.com>
2025-08-31 23:41:27 +0530

    fix: go-app_error-log

--- go-app/main.go
+++ go-app/main.go
@@ -18,5 +18,7 @@ func main() {
     http.HandleFunc("/", home)
     http.HandleFunc("/test", test)
     fmt.Println("Server running on port 8080")
-    http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
